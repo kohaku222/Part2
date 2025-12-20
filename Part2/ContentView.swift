2cbd05af717464e40c20dfd82ecd930d4f1b7cf9
kohaku222 <ikeda-sho1218@g.ecc.u-tokyo.ac.jp>
2025-12-20 17:21:01 +0900

    Initial Commit

---
+++ Part2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Part2
+//
+//  Created by 池田　聖 on 2025/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Part2/Part2App.swift
@@ -0,0 +1,17 @@
+//
+//  Part2App.swift
+//  Part2
+//
+//  Created by 池田　聖 on 2025/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Part2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
